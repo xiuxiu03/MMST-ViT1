48498721a2285c069961cfe8c72505171eb92136
xiuxiu03 <599360191@qq.com>
2025-06-03 22:47:17 +0800

    Update data_wrapper.py

--- dataset/data_wrapper.py
+++ dataset/data_wrapper.py
@@ -60,7 +60,7 @@ class ScalarNorm(object):
 
 
 if __name__ == '__main__':
-    root_dir = "/mnt/data/common_data/multi_modal"
+    root_dir = "/mnt/data/common_data/multi_modal/Tiny-CropNet"
     train = "./data/soybean_train.json"
     dataset = Sentinel_Dataset(root_dir, train)
     train_loader = torch.utils.data.DataLoader(dataset, batch_size=1)
