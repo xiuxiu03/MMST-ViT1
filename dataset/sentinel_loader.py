9e93cd927b1b20a7062ec92ba2ff7fd5c708872a
xiuxiu03 <599360191@qq.com>
2025-06-03 22:38:49 +0800

    Update sentinel_loader.py

--- dataset/sentinel_loader.py
+++ dataset/sentinel_loader.py
@@ -54,9 +54,8 @@ class Sentinel_Dataset(Dataset):
 
 
 if __name__ == '__main__':
-    root_dir = "/mnt/data/Tiny CropNet"
-    # train = "./../data/soybean_train.json"
-    train = "./../data/soybean_val.json"
+    root_dir = "/mnt/data/common_data/multi_modal"
+    train = "./data/soybean_train.json"
     dataset = Sentinel_Dataset(root_dir, train)
     train_loader = torch.utils.data.DataLoader(dataset, batch_size=1)
 
