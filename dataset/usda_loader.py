729b0c6129bd8a98e50deed43c76928fe0086bd1
xiuxiu03 <599360191@qq.com>
2025-06-03 22:48:34 +0800

    Update usda_loader.py

--- dataset/usda_loader.py
+++ dataset/usda_loader.py
@@ -64,7 +64,7 @@ class USDA_Dataset(Dataset):
 
 
 if __name__ == '__main__':
-   root_dir = "/mnt/data/common_data/multi_modal"
+   root_dir = "/mnt/data/common_data/multi_modal/Tiny-CropNet"
     train = "./data/soybean_train.json"
     # train = "./../data/soybean_train.json"
     
